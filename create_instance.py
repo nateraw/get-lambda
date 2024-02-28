4ef98843e2a5e13e418e44da30c0b7fc00ded631
Nathan Raw <nxr9266@g.rit.edu>
2024-02-28 17:15:46 -0600

    Update create_instance.py

--- create_instance.py
+++ create_instance.py
@@ -4,13 +4,13 @@ from lambdacloud import login
 import os
 import time
 
-def main(instance_type):
+def main(instance_type, file_system_names=None):
     sleep_time = 1
     login(token=os.getenv('LAMBDA_CLOUD_TOKEN'))
 
     while True:
         try:
-            instance_id = create_instance(instance_type, ssh_key_names="lambda-new")
+            instance_id = create_instance(instance_type, ssh_key_names="lambda-new", file_system_names=file_system_names)
             print(f'Instance created with id: {instance_id}')
             break
         except:
@@ -20,5 +20,6 @@ def main(instance_type):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--instance_type", type=str, required=True, help="Instance type to be created.")
+    parser.add_argument("--file_system_names", type=str, default=None, required=False, help="File system to attach.")
     args = parser.parse_args()
     main(args.instance_type)
